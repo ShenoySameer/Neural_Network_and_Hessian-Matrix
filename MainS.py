9bf29c125ffc505dbe484517bd88b31925b9ad42
SameerShenoy <champyshenoy@gmail.com>
2024-06-03 08:48:35 -0700

    It works!!!!

--- MainS.py
+++ MainS.py
@@ -1,64 +1,49 @@
-from sklearn.datasets import load_digits
 import numpy as np
+from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-# digits = load_digits()
-#
-# # The data and target are already NumPy arrays, but let's explicitly convert them
-# data = np.array(digits.data)
-# target = np.array(digits.target)
-#
-# X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=15)
-#
-# X_train = X_train.T
-# X_test = X_test.T
-# y_train = y_train  # y's are vectors, no need to transpose.
-# y_test = y_test
+digits = load_digits()
+data = np.array(digits.data)
+target = np.array(digits.target)
 
-class Layer:
-    def __init__(self):
-        self.input = None
-        self.output = None
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=15)
 
-    def forward(self, input):
-        # TODO: return output
-        pass
+X_train = X_train.T
+X_test = X_test.T
 
-    def backward(self, output_gradient, learning_rate):
-        # TODO: update parameters and return input gradient
-        pass
+# Normalize the input data
+X_train = X_train / 16.0
+X_test = X_test / 16.0
 
-class Dense(Layer):
+y_train = y_train  # y's are vectors, no need to transpose.
+y_test = y_test
+
+print(X_train.shape)
+X_train = np.reshape(X_train, (64, 1437, 1))
+
+class Dense():
     def __init__(self, input_size, output_size):
-        '''output size is the next layer dimension, input size is how much data it is taking in'''
-        self.weights = np.random.randn(output_size, input_size)
-        self.bias = np.random.randn(output_size, 1)
+        self.weights = np.random.randn(output_size, input_size) * np.sqrt(2. / input_size)  # He initialization
+        self.bias = np.zeros((output_size, 1))
 
     def forward(self, input):
         self.input = input
         return np.dot(self.weights, self.input) + self.bias
 
     def backward(self, output_gradient, learning_rate):
-
-        #find find the change in weights by getting the dot product of the derivative of the output with the tranpose of input
-        #after it takes this gradient multiplies by the learning rate and subtracts it from the current weights
         weights_gradient = np.dot(output_gradient, self.input.T)
-
-        #the error with respect to x is dot product of weights transpose by derivaitve of error with respsect to the output
         input_gradient = np.dot(self.weights.T, output_gradient)
         self.weights -= learning_rate * weights_gradient
-
-        #change bias is given by the derivative of the output
         self.bias -= learning_rate * output_gradient
-
         return input_gradient
 
-class Activation(Layer):
+class Activation():
     def __init__(self, activation, activation_prime):
-        super().__init__()
         self.activation = activation
         self.activation_prime = activation_prime
+        self.input = None
+        self.output = None
 
     def forward(self, input):
         self.input = input
@@ -67,44 +52,28 @@ class Activation(Layer):
     def backward(self, output_gradient, learning_rate):
         return np.multiply(output_gradient, self.activation_prime(self.input))
 
-
-
 class Leaky_ReLU(Activation):
     def __init__(self, a=0.01):
         self.a = a
         lr = lambda x: np.maximum(self.a * x, x)
-        lr_prime = lambda x: np.where(x > 0, 1, np.where(x < 0, self.a, 0))
+        lr_prime = lambda x: np.where(x > 0, 1, self.a)
         super().__init__(lr, lr_prime)
-
-
-
-def get_predictions(A2):
-    return np.argmax(A2, 0)
-
-
-def get_accuracy(predictions, y):
-    return np.sum(predictions == y) / y.size
-
 
 def mse(y_true, y_pred):
     return np.mean(np.power(y_true - y_pred, 2))
 
-
 def mse_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / np.size(y_true)
-
-X_train = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
-y_train = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
+    return 2 * (y_pred - y_true) / y_true.size
 
 network = [
-    Dense(2, 3),
+    Dense(1437, 16),
     Leaky_ReLU(),
-    Dense(3, 1),
+    Dense(16, 10),
     Leaky_ReLU()
 ]
 
 iterations = 10000
-learning_rate = 0.1
+learning_rate = 0.001  # Reduced learning rate
 
 for i in range(iterations):
     cost = 0
@@ -115,21 +84,22 @@ for i in range(iterations):
             output = layer.forward(output)
 
         cost += mse(y, output)
+        gradient = mse_prime(y, output)
+        for layer in reversed(network):
+            gradient = layer.backward(gradient, learning_rate)
 
-    gradient = mse_prime(y, output)
-    for layer in reversed(network):
-        gradient = layer.backward(gradient, learning_rate)
-
-    #I have no idea what this does
-    cost /= len(x)
-    if (i+1)%1000 == 0:
-        print(i+1, cost)
+    cost /= len(X_train)
+    if (i+1) % 10 == 0:
+        print(f"Iteration {i+1}, Cost: {cost}")
+        if np.isnan(cost):
+            print("NaN encountered!")
+            break
 
 def applyNeuralNetwork(network, X):
     for layer in network:
         X = layer.forward(X)
     return X
 
-
-test_input = np.reshape([[0, 0]], (2, 1))  # Example input
+test_input = np.reshape([[1, 0]], (2, 1))  # Example input
 print(f'Input: {test_input.flatten()} Gives output: {applyNeuralNetwork(network, test_input)}')
+
